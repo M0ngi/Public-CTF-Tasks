b98b5e28009155a41073d1aed4e9aa2c9a344903
M0ngi <saidanemongi@gmail.com>
2023-03-22 23:59:17 +0100

    Add solver

---
+++ 2023/NCSC/src/pycryptojail/solver.py
@@ -0,0 +1,17 @@
+from Crypto.Util.number import bytes_to_long
+from pwn import *
+from sympy import nextprime
+
+r = process(["python3.10", "main.py"])
+
+payload = b"[c:=getattr(open('fl\\141g'),'read')()]" + b"a"*50
+payload = bytes_to_long(payload)
+payload = nextprime(payload)
+
+p = payload
+x = payload-1
+
+r.sendline(str(x))
+r.sendline(str(p))
+
+r.interactive()
